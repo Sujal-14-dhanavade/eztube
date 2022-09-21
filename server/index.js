d803ee2a50289bc6df21b5507ad3658d945ca054
Surojeet <surojeetghosh89@gmail.com>
2022-09-21 21:59:56 +0530

    Basic setup of express and nextjs for ezTube

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const next = require('next');
+const homeRoute = require('./routes/home');
 
 const dev = process.env.NODE_ENV !== 'production'
 const port = process.env.PORT || 3000;
-const app = next({});
+const app = next({dir: ".", dev});
 const handle = app.getRequestHandler();
 
 app.prepare().then( () => {
-    
+    const server = express();
+    server.use("/api", homeRoute);
+    server.get("*", (req, res) => {
+        return handle(req, res);
+    });
+    server.listen(port, () => {
+        console.log("Server Started");
+    })
 })
