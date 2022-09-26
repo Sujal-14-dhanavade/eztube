092c5b98f606f633fa3e53aade91c012f680e27d
Surojeet <surojeetghosh89@gmail.com>
2022-09-26 22:01:33 +0530

    Setup mongodb connection need to check video upload

--- server/index.js
+++ server/index.js
@@ -1,12 +1,36 @@
+require('dotenv').config();
+// requiring modules
+
 const express = require('express');
 const next = require('next');
 const homeRoute = require('./routes/home');
+const mongoose = require('mongoose');
 
+// connecting to database
+const database = "mongodb+srv://surojeet:" + process.env.ATLAS_PASSWORD +"@cluster0.qhibn.mongodb.net/EzTube?retryWrites=true&w=majority";
+mongoose.connect(database)
+    .then(() => {
+        console.log("Database Connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+
+
+// starting development mode
 const dev = process.env.NODE_ENV !== 'production'
+// selecting port
 const port = process.env.PORT || 3000;
+// next app
 const app = next({dir: ".", dev});
+// handler to handle all requests
 const handle = app.getRequestHandler();
 
+
+
+
+// preparing next app to connect to express server as backend
 app.prepare().then( () => {
     const server = express();
     server.use("/api", homeRoute);
--- server/routes/home.js
+++ server/routes/home.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const mongoose = require("mongoose");
+const videoSchema = require("../models/videoModel");
+
 
 const router = express.Router();
 
+const video = mongoose.model("video",videoSchema )
 router.route('/')
 .get((req, res) => {
-    res.send("Hello from express");
+    const data = new video(
+        {
+            video_title: "Time to see",
+            video_time:24000,
+        }
+    )
+    data.save().then(()=> {
+        console.log("Saved")
+    });
 })
 
 module.exports = router;
