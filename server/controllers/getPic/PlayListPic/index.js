651eff298096324a4f60ad42d6becb370cd144a5
Surojeet Ghosh <surojeetghosh89@gmail.com>
2022-10-26 14:32:47 +0530

    Controller and routes were added for storages, for getting data

---
+++ server/controllers/getPic/PlayListPic/index.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+const getPlaylist = async (id) => {
+  let gfs = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
+    bucketName: "playlistAvatar",
+  });
+  const file = await gfs.find({ _id: id }).toArray();
+  return file[0];
+};
+
+const streamPlaylistPic = (req, res) => {
+  let gfs = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
+    bucketName: "playlistAvatar",
+  });
+  const Objectid = mongoose.Types.ObjectId(req.params.playlistPic);
+  getPlaylist(Objectid).then((file) => {
+    if (!file && file.length === 0) {
+      res.status(400).send("No file found");
+    }
+    const range = req.headers.range;
+    if (range) {
+      let size = file.length;
+      let [start, end] = range.replace(/bytes=/, "").split("-");
+      start = Number(start);
+      end = end ? Number(end) : size - 1;
+
+      if (!isNaN(start) && isNaN(end)) {
+        end = size - 1;
+      }
+      if (isNaN(start) && !isNaN(end)) {
+        start = size - end;
+        end = size - 1;
+      }
+      if (start >= size || end >= size) {
+        res.writeHead(416, {
+          "Content-Range": `bytes */${size}`,
+        });
+        return res.end();
+      }
+      res.writeHead(206, {
+        "Content-Range": `bytes ${start}-${end}/${size}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": end - start + 1,
+        "Content-Type": "image/jpeg",
+      });
+      gfs
+        .openDownloadStream(Objectid, {
+          start: start,
+          end: end,
+        })
+        .pipe(res);
+    } else {
+      return res.status(500).send("No ranges for headers");
+    }
+  });
+}
+module.exports.getPlaylistPic = streamPlaylistPic;
