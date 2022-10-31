c4ac1fd6004e779d7afb2755b80dc67a6ca1bcd2
Surojeet Ghosh <surojeetghosh89@gmail.com>
2022-10-31 17:00:43 +0530

    Completed email validation 1st step for registeration

--- components/CollapseMenu/SignIn/index.js
+++ components/CollapseMenu/SignIn/index.js
@@ -4,22 +4,22 @@ import {TextField, Typography} from '@mui/material';
 
 export default function SignIn() {
   return (
-    <div class="modal fade text-dark" id="signInForm">
-      <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-          <div class="modal-header text-center">
-            <h4 class="modal-title w-100 font-weight-bold">Sign in</h4>
+    <div className="modal fade text-dark" id="signInForm">
+      <div className="modal-dialog modal-dialog-centered">
+        <div className="modal-content">
+          <div className="modal-header text-center">
+            <h4 className="modal-title w-100 font-weight-bold">Sign in</h4>
             <button
               type="button"
-              class="btn-close"
+              className="btn-close"
               data-bs-dismiss="modal"
               aria-label="Close"
             ></button>
           </div>
-          <div class="modal-body">
+          <div className="modal-body">
             <form action="/" method="POST" className="text-center">
-                <TextField className="mx-2" fullWidth variant="standard" type={"text"} label={<i class="fa-solid fa-user"></i>} required name="username"/>
-                <TextField className="mx-2 mt-4" fullWidth variant="standard" type={"password"} label={<i class="fa-solid fa-key"></i>} name="password" required/>
+                <TextField className="mx-2" fullWidth variant="standard" type={"text"} label={<i className="fa-solid fa-user"></i>} required name="username"/>
+                <TextField className="mx-2 mt-4" fullWidth variant="standard" type={"password"} label={<i className="fa-solid fa-key"></i>} name="password" required/>
                 <button type="submit" className="my-5 btn btn-danger w-100">Continue</button>
                 <Typography variant="caption" className="d-block">When registering, you agree that we may use your provided data for the registration and to send you notifications on our products and services. You can unsubscribe from notifications at any time in your settings. For additional info please refer to our Privacy Policy.</Typography>
             </form>
--- components/CollapseMenu/SignUp/index.js
+++ components/CollapseMenu/SignUp/index.js
@@ -1,24 +1,48 @@
 import React from "react";
 import {TextField, Typography} from '@mui/material';
+import IsEmail from "isemail";
 
 
-export default function SignUp() {
+export default function SignUp(props) {
+  const [email, changeEmail] = React.useState({
+    data: null,
+    error: false,
+    helper: ""
+  });
+
+  function emailChange(e) {
+    if (IsEmail.validate(e.target.value, {errorLevel: false}) === true) {
+      changeEmail({data: e.target.value, error: false ,helper: ""});
+    } else {
+      changeEmail({data: e.target.value, error: true, helper: ""});
+    }
+  }
+
+  function validate(e) {
+    e.preventDefault();
+    if(email.error === true) {
+      changeEmail({...email,helper: "Email is not valid" });
+    } else {
+      props.changeReg({...props.reg, email: email.data});
+    }
+  }
+
   return (
-    <div class="modal fade text-dark" id="signUpForm">
-      <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-          <div class="modal-header text-center">
-            <h4 class="modal-title w-100 font-weight-bold">Sign up</h4>
+    <div className="modal fade text-dark" id="signUpForm">
+      <div className="modal-dialog modal-dialog-centered">
+        <div className="modal-content">
+          <div className="modal-header text-center">
+            <h4 className="modal-title w-100 font-weight-bold">Sign up</h4>
             <button
               type="button"
-              class="btn-close"
+              className="btn-close"
               data-bs-dismiss="modal"
               aria-label="Close"
             ></button>
           </div>
-          <div class="modal-body">
-            <form action="/" method="POST" className="text-center">
-                <TextField className="mx-2 mt-4" fullWidth variant="standard" type={"email"} label={"Your email address"} required name="email"/>
+          <div className="modal-body">
+            <form className="text-center" onSubmit={validate}>
+                <TextField className="mx-2 mt-4" fullWidth variant="standard" error={email.error} type={"email"} label={"Your email address"} required name="email" value={email.data} onChange={emailChange} />
                 <button type="submit" className="my-5 btn btn-danger w-100">Continue</button>
                 <Typography variant="caption" className="d-block">When registering, you agree that we may use your provided data for the registration and to send you notifications on our products and services. You can unsubscribe from notifications at any time in your settings. For additional info please refer to our Privacy Policy.</Typography>
             </form>
