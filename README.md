7e38b1625a937f52361ae99a917f9dd7853b7d28
Surojeet Ghosh <surojeetghosh89@gmail.com>
2022-11-05 22:58:38 +0530

    login form validation done just need to check and authenticate

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ezTube
 A simple and easy to use video streaming web app developed using MERN stack.
 
-Register page :- assign userpic uploaded id to user id in req, res of /api/userApiUploaded
+Login remaining
--- components/CollapseMenu/SignIn/index.js
+++ components/CollapseMenu/SignIn/index.js
@@ -1,8 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
 import {TextField, Typography} from '@mui/material';
+import { Router, useRouter } from "next/router";
+import axios from "axios";
 
 
 export default function SignIn() {
+  const [data, changeData] = React.useState({
+    username: "",
+    password: ""
+  });
+
+  const router = useRouter();
+
+
+  const [error, setError] = React.useState("");
+  const isEmpty = () => {
+    for (var i in data) {
+      if (data[i] === "") return true;
+    }
+    return false;
+  };
+
+  const onChange = (e) => {
+    changeData({ ...data, [e.target.id]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const regPass =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if (isEmpty()) {
+      setError("Empty Fields!! Please fill the form");
+    } else if (data.username.length < 7) {
+      setError("Username should be contain more than 6 characters");
+    } else if (!data.password.match(regPass)) {
+      setError(
+        "Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character"
+      );
+    } else {
+      axios
+        .request({
+          method: "POST",
+          url: "/api/login",
+          data: data,
+        })
+        .then((res) => {
+          if (res.data._id) {
+            router.push("/Ezport");
+          } else {
+            setError("Server Error");
+          }
+        });
+    }
+  };
   return (
     <div className="modal fade text-dark" id="signInForm">
       <div className="modal-dialog modal-dialog-centered">
@@ -16,10 +66,11 @@ export default function SignIn() {
             ></button>
           </div>
           <div className="modal-body">
-            <form action="/" method="POST" className="text-center">
-                <TextField className="mx-2" fullWidth variant="standard" type={"text"} label={<i className="fa-solid fa-user"></i>} required name="username"/>
-                <TextField className="mx-2 mt-4" fullWidth variant="standard" type={"password"} label={<i className="fa-solid fa-key"></i>} name="password" required/>
+            <form className="text-center" onSubmit={onSubmit}>
+                <TextField onChange={onChange} value={data.username} className="mx-2" fullWidth variant="standard" type={"text"} label={<i className="fa-solid fa-user"></i>} required name="username" id="username"/>
+                <TextField onChange={onChange} value={data.password} className="mx-2 mt-4" fullWidth variant="standard" type={"password"} label={<i className="fa-solid fa-key"></i>} name="password" id="password" required/>
                 <button type="submit" className="my-5 btn btn-danger w-100">Continue</button>
+                <Typography variant="caption" className="d-block text-danger">{error}</Typography>
                 <Typography variant="caption" className="d-block">When registering, you agree that we may use your provided data for the registration and to send you notifications on our products and services. You can unsubscribe from notifications at any time in your settings. For additional info please refer to our Privacy Policy.</Typography>
             </form>
           </div>
