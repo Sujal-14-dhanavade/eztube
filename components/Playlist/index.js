4748d1be541d6e15d097658ed4953bf4750a505e
Surojeet Ghosh <surojeetghosh89@gmail.com>
2022-11-13 15:28:01 +0530

    Getting Playlist from server

--- components/Playlist/index.js
+++ components/Playlist/index.js
@@ -1,9 +1,16 @@
 import { Typography } from "@mui/material";
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 import Playlistform from "./Playlistform";
 
 export default function Playlist(props) {
 
+  useEffect(() => {
+    axios.request({
+      method: "GET",
+      url: "/api/getPlaylist"
+    }).then((res) => {console.log(res.data)});
+  }, [])
   return (
     <div className="mt-3">
       <Typography className="text-center" variant="h4"><i class="fa-brands fa-napster me-3"></i>Playlist</Typography>
